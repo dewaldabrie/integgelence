14138ca0c53bb408065088f887ca2b23d7ac4405
Dewald Abrie <dewaldabrie@gmail.com>
2017-10-08 22:45:30 +1100

    Add Tamagotchi state update and health calcs
    Two matrixes (A and B) take care of describing how the input
    affects the animal state. Health metrics are then calcualted from the
    state.
    
    Health = weights (dot) state
    The weights are normalized to sum to 1 before doing the dot product.
    
    Added tests for health calcs and state update

--- tamagotchi/base.py
+++ tamagotchi/base.py
@@ -1,6 +1,26 @@
 import numpy as np
 from math import log
 
+# input affects Tamagotchi's internal state
+ALLOWABLE_INPUTS = {
+            'feed',
+            'pet',
+            'excercise',
+            'immunize',
+            'clean',
+        }
+
+# interactions affect Tamagotchi's active state
+ALLOWABLE_INTERACTIONS = {
+            'put to bed', # TODO
+            'water',
+            'petting',
+            'excercise',
+            'immunization shot', # TODO
+            'shelter', # TODO
+        }
+
+
 class Tamagotchi:
     """
     Define the base attributes and methods of a Tamagotchi.
@@ -8,30 +28,200 @@ class Tamagotchi:
     # general attributes
     species_name = 'Tamagotchi'
 
+    # possible active states
+    ACTIVE_STATES = {
+        'sleeping',
+        'inactive',
+        'playing',
+        'eating',
+        'pooping'
+    }
+
+    AGE_STATES = {
+        'infant',
+        'todler',
+        'teenager',
+        'adult',
+        'aged'
+    }
+
     # physical attributes
     adult_mean_weight = 100  # kg
+    weight = adult_mean_weight
     age = 1./365  # years
 
     # parameters
-    #  takes on values between -1.0 and 1.0 (inclusive)
+    #  takes on values between 0 and 1.0 (inclusive)
     #  mental
     social_affinity = 0.5
     touch_affinity = 0.5
     excercise_affinity = 0.5
-    theta_mental = np.array([
-        social_affinity,
-        touch_affinity,
-        excercise_affinity,
-    ])
+    food_affinity = 1.0  # always thinks it's hungry
 
     #  physical
-    appetite = lambda age: 0.5 + np.log(age) / log(1. / 365) * 0.5  # appetite drops with age
-    immune_function = 0.5
+    @property
+    def appetite(self):
+        """
+        Appetite drops with age
+        See notebooks/InteractionModel.ipynb
+        """
+        return 0.5 + np.log(self.age) / log(1. / 365) * 0.5
+
+    disease_succeptibility = 0.5
     weather_resistance = 0.5
     excercise_required = 0.5
-    theta_phys = np.array([
-        immune_function,
-        weather_resistance,
-        excercise_required
+
+
+    # state
+    # perfect/healthy state variable has value of zero
+    nurishment = -0.5  # hungry <--> overfed
+    social = -0.5  # lonely <--> annoid
+    fitness = -0.5  # unfit <--> chronic exhaustion
+    undiseased = 0  # terminally diseased <--> auto-immune disorder
+    state_dict = {
+        'nurishment': nurishment,
+        'social': social,
+        'fitness': fitness,
+        'undiseased': undiseased,
+    }
+
+    # Create the transformation matrix T to map inputs to state
+    #         Input vector : (n,1)
+    #         State vector: (m,1)
+    #         n is stricly larger or equal to m
+    #         Transformation matrix:(m,n)x(n,1) => (m,1)
+    #         Transformation matrix such that Snext = Sprev + Sprev (dot) T x I
+    #
+
+    # (4,m) x (m,n) x (nx1)
+    #  B       A       I
+
+    # Attribute to state mapping
+    # 4xm
+    # rows have to sum to 1
+    #   food_affinity  touch_affinity  social_affinity  excercise_affinity  appetite  excercise_required  disease_succeptibility
+    B = np.matrix([
+        [0.5,          0.0,             0.0,             0.0,                0.5,     0.0,                 0.0,               ],  # nourishment
+        [0.0,          0.5,             0.5,             0.0,                0.0,     0.0,                 0.0,               ],  # social
+        [0.0,          0.0,             0.0,             0.5,                0.0,     0.5,                 0.0,               ],  # fitness
+        [0.0,          0.0,             0.1,             0.2,                0.2,     0.0,                 0.5,               ],  # undiseased
     ])
 
+    # Input to attribute mapping
+    # mxn
+    # rows have to sum to 1
+    #   'feed', 'pet', 'excercise', 'immunize', 'clean'
+    A = np.matrix([
+        [1.0,    0.0,    0.0,          0.0,        0.0],    # food_affinity
+        [0.0,    1.0,    0.0,          0.0,        0.0],    # touch_affinity
+        [0.0,    0.5,    0.5,          0.0,        0.0],    # social_affinity
+        [0.0,    0.0,    1.0,          0.0,        0.0],   # excercise_affinity
+        [1.0,    0.0,    0.0,          0.0,        0.0],   # appetite
+        [0.0,    0.0,    1.0,          0.0,        0.0],   # excercise_required
+        [0.0,    0.0,    0.0,          0.1,        0.9],   # disease_succeptibility
+    ])
+
+    # health state vector
+    state = np.array([
+        nurishment,
+        social,
+        fitness,
+        undiseased,
+    ])
+
+    # health properties
+    # this represents relative importance of the various state variables
+    # to the type of health in question
+    # these must correspond to the state variables in the same order
+    # (nx1) dot (nx1) => (1x1)
+    mental_health_map = {
+        'nourishment' : food_affinity,
+        'social': (touch_affinity + social_affinity)/2.,
+        'fitness': excercise_affinity,
+        'undiseased': .5,
+    }
+    mental_health_weights = np.array([
+        food_affinity, # nourishment
+        (touch_affinity + social_affinity)/2., # social
+        excercise_affinity, # fitness
+        0.5, # undiseased
+    ])
+
+    physical_health_map = {
+        'nourishment': appetite,
+        'social': 0.,
+        'fitness': excercise_required,
+        'undiseased': disease_succeptibility,
+    }
+    physical_health_weights = np.array([
+        appetite, # average size needs average quantity of food
+        0.0,
+        excercise_required,
+        disease_succeptibility,
+    ])
+
+    @property
+    def mental_health(self):
+        return self._calc_health(self.mental_health_weights)
+
+    @property
+    def physical_health(self):
+        return self._calc_health(self.physical_health_weights)
+
+    def _calc_health(self, weights):
+        """
+        Calculate heath based on the relevant weights and the internal state
+
+        Perfect state variables are zero, but for the sake of human understanding,
+        perfect health is represented as unity.
+
+        Health = 1 - normalized_weights (dot) abs(state)
+
+        The abs val of the state is taken because both extremes are bad, for
+        example over-eating and under-eating
+        """
+        # normalize the selection weights
+        norm_weights = self.normalize(weights)
+        # calculate health
+        health = 1.0 - np.dot(
+            norm_weights,
+            abs(self.state)
+        )
+        return health
+    # energy properties
+    @staticmethod
+    def normalize(vector:np.array) -> np.array:
+        """
+        Cause non-zero weight vectors to sum to 1.
+        :param vector: numpy array with relative weights that indicate relative importance of corresponding values in
+        another array that the selection vector will be dotted with
+        :return: array that sums to 1, except zero array passes through
+        """
+        # make sure all values are positive
+        if vector.min() < 0.:
+            vector += vector.min()
+
+        s = vector.sum()
+        return vector/s if s > 0 else vector
+
+    def receive_input(self, input:dict) -> None:
+        """
+        Recieve an input from another agent and update the
+        state (S) as required.
+
+        Snext += B x A x I
+        """
+        input_mat = self._parse_input(input)
+
+        self.state = self.state + np.squeeze(self.B*self.A*input_mat)
+
+    def _parse_input(self, input_dict:dict) -> np.matrix:
+        """
+        Parse the input dictionary into an array with dimensions same as that
+        of ALLOWABLE_INPUTS
+
+        :return: matrix of dimension nx1 (n being max number of inputs)
+        """
+        result_dict = dict((name, 0.) for name in ALLOWABLE_INPUTS)
+        result_dict.update(input_dict)
+        return np.matrix(list(result_dict.values())).T
---
+++ tamagotchi/tests/test_base.py
@@ -0,0 +1,117 @@
+import unittest
+import numpy as np
+from tamagotchi.base import Tamagotchi, ALLOWABLE_INPUTS, ALLOWABLE_INTERACTIONS
+
+class TestTamagotchiBase(unittest.TestCase):
+    """Unit tests fort the Tamagotchi base class"""
+    def setUp(self):
+        self.t = Tamagotchi()
+
+    def test_state_change_calc(self):
+        """
+        If an input array is passed to the Tamagotchi, does it update it's state
+        correctly?
+        """
+        # create mock input - supply every possible need
+        fake_input = {
+            'feed': 1.0,
+            'pet': 1.0,
+            'excercise': 1.0,
+            'immunize': 1.0,
+            'clean': 1.0,
+        }
+
+        # set state to average before
+        self.t.state = np.zeros(len(self.t.state), dtype=np.float64)
+
+        # calculate state change based on fake input
+        self.t.receive_input(fake_input)
+
+        print(self.t.state)
+        self.assertTrue((self.t.state == np.ones(4, dtype=np.float64)).all())
+
+
+    def test_parse_input(self):
+        """
+        Does _parse_input preserve the order of ALLOWED_INPUTS?
+        """
+        input_dict = {
+            'feed': 1.0,
+            'pet': 1.0,
+            'excercise': 1.0,
+            'immunize': 1.0,
+            'clean': 1.0,
+        }
+
+        parsed_input = self.t._parse_input(input_dict)
+
+        self.assertEqual(parsed_input.shape, (len(ALLOWABLE_INPUTS), 1))
+
+    def test_normalize(self):
+        """
+        Can a vector be normalized to sum to 1.0?
+        """
+        input = np.array([1,1,1])
+        self.assertFalse(input.sum() == 1.0)
+
+        res = self.t.normalize(input)
+        self.assertTrue(res.sum() == 1.0, msg=f"Answer is {res}, instead of 1.0")
+
+    def test_normalize_on_zeros(self):
+        """
+        If a zero vector is normalized, does it sum to 0.0?
+        """
+        input = np.zeros(5)
+        self.assertAlmostEqual(input.sum(), 0.0, places=4)
+
+        res = self.t.normalize(input)
+        self.assertTrue(res.sum() == 0.0, msg=f"Answer is {res}, instead of 0.0")
+
+    def test_normalize_on_negatives(self):
+        """
+        If a zero vector contains negatives , does it sum to 1.0?
+        """
+        input = np.zeros(5) * -1
+        self.assertFalse(input.sum() == 1.0)
+
+        res = self.t.normalize(input)
+        self.assertTrue(res.sum() == 0.0, msg=f"Answer is {res}, instead of 0.0")
+
+    def test_health_calcs(self):
+        """
+        Can mental health be calculated from the state and weights vector?
+        """
+        # create unity state
+        self.t.state = np.ones(5, dtype=np.float64)
+        # create unity weights
+        self.t.mental_health_weights = np.ones(len(self.t.state), dtype=np.float64)
+        self.t.physical_health_weights = self.t.mental_health_weights
+
+        mental_health = self.t.mental_health
+        physical_health = self.t.physical_health
+
+        self.assertEqual(mental_health, 1.0)
+        self.assertEqual(physical_health, 1.0)
+
+    def test_internal_health_calc(self):
+        # create 0.5 state
+        self.t.state = np.ones(5, dtype=np.float64)*0.5
+        # create unity weights
+        weights = np.ones(len(self.t.state), dtype=np.float64)
+
+        self.t._calc_health(weights)
+
+        self.assertEqual(self.t._calc_health(weights), 0.5)
+
+        # create 0.5 state
+        self.t.state = np.ones(5, dtype=np.float64) * 0.5
+        # create 0.5 weights
+        # Note: Because the weights only indicate relative importance,
+        # these will be normalized to sum to 1, so is the same as a
+        # unity vector.
+        # Therefore the result should be the same
+        weights = np.ones(len(self.t.state), dtype=np.float64) * 0.5
+
+        self.t._calc_health(weights)
+
+        self.assertEqual(self.t._calc_health(weights), 0.5)
